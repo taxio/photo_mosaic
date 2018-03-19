d851381419abccad958f5355437297c22db35139
taxio <taxio.2029@gmail.com>
2018-03-19 17:55:22 +0900

    透過画像に対応

--- test/calc_mean.py
+++ test/calc_mean.py
@@ -5,6 +5,10 @@ import glob
 if __name__ == '__main__':
     img_names = glob.glob('./images/*')
     for img_name in img_names:
-        img = Image.open(img_name).convert("RGB")
+        img = Image.open(img_name)
+        # 透過画像の場合は一度RGBAに変換
+        if "transparency" in img.info:
+            img = img.convert("RGBA")
+        img = img.convert("RGB")
         stat = ImageStat.Stat(img)
         print(img_name, stat.mean)
