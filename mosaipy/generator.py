7875704b8bfcebbda5a5cf8680f5a3b3ef74d572
taxio <taxio.2029@gmail.com>
2018-03-23 04:57:30 +0900

    refactor

--- mosaipy/generator.py
+++ mosaipy/generator.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageStat
 import numpy as np
 import sqlite3
+from progressbar import ProgressBar
 
 
 class PhotoMosaicGenerator:
@@ -24,36 +25,27 @@ class PhotoMosaicGenerator:
         target_image = np.array(target_image, 'f')
         print(self._target, target_image.shape)
 
-        # means = list()  # 分割したRGB値平均値がここに入る
-        materials = list()
+        pbar = ProgressBar(max_value=n_split*n_split)
+        gen_image = Image.new('RGB', target_image_size, 'white')
         v_imgs = np.array_split(target_image, n_split, axis=1)
-        for v in v_imgs:
-            tmp = list()
-            material = list()
+        for n_v, v in enumerate(v_imgs):
             h_imgs = np.array_split(v, n_split, axis=0)
-            for h in h_imgs:
+            for n_h, h in enumerate(h_imgs):
+                # 対象画像の平均値計算
                 tmp_mean = np.mean(h, axis=0)
                 tmp_mean = np.mean(tmp_mean, axis=0)
+                # 近似画像取得
                 mat_img = self.get_near_image(tmp_mean[0], tmp_mean[1], tmp_mean[2])
-                tmp.append(tmp_mean)
-                print(mat_img)
-                material.append(mat_img)
-
-            # means.append(tmp)
-            materials.append(material)
-
-        # 画像貼り付け
-        gen_image = Image.new('RGB', target_image_size, 'white')
-        for n_v, mats in enumerate(materials):
-            for n_h, mat in enumerate(mats):
-                mat_image = Image.open(mat)
+                # 近似画像を指定位置に貼り付け
+                mat_image = Image.open(mat_img)
                 if "transparency" in mat_image.info:
                     mat_image = mat_image.convert("RGBA")
                 mat_image = mat_image.convert("RGB")
                 mat_image = self.trim_into_square(mat_image)
                 mat_image = mat_image.resize((mat_size, mat_size))
-                print(n_v, n_h, mat, mat_image.size)
                 gen_image.paste(mat_image, (n_v*mat_size, n_h*mat_size))
+                # print(n_v, n_h, mat_img, mat_image.size)
+                pbar.update(n_v*n_split+n_h)
 
         gen_image.save('generated.png')
                 
