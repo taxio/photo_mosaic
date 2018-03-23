a2258303e7fcb02758a1e6bdc595013e2ca096a1
taxio <taxio.2029@gmail.com>
2018-03-23 13:16:08 +0900

    共通の処理をutil.pyにまとめた

--- mosaipy/generator.py
+++ mosaipy/generator.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageStat
 import numpy as np
 import sqlite3
 from progressbar import ProgressBar
+import mosaipy.util
 
 
 class PhotoMosaicGenerator:
@@ -14,7 +15,7 @@ class PhotoMosaicGenerator:
         self._materials = c.fetchall()
         conn.close()
 
-    def generate(self, target_image_name: str, n_split: int, output_image_name: str):
+    def generate(self, target_image_name: str, n_split: int, output_image_name: str, threshold_near=5):
         print('generating photo mosaic...')
 
         target_image = Image.open(target_image_name)
@@ -33,47 +34,19 @@ class PhotoMosaicGenerator:
                 tmp_mean = np.mean(h, axis=0)
                 tmp_mean = np.mean(tmp_mean, axis=0)
                 # 近似画像取得
-                mat_img = self.get_near_image(tmp_mean[0], tmp_mean[1], tmp_mean[2])
+                mat_img = self.get_near_image(tmp_mean[0], tmp_mean[1], tmp_mean[2], threshold=threshold_near)
                 # 近似画像を指定位置に貼り付け
                 mat_image = Image.open(mat_img)
-                if "transparency" in mat_image.info:
-                    mat_image = mat_image.convert("RGBA")
-                mat_image = mat_image.convert("RGB")
-                mat_image = self.trim_into_square(mat_image)
+                mat_image = mosaipy.util.convert_to_rgb_image(mat_image)
+                mat_image = mosaipy.util.trim_into_square(mat_image)
                 mat_image = mat_image.resize((mat_size, mat_size))
                 gen_image.paste(mat_image, (n_v*mat_size, n_h*mat_size))
                 # print(n_v, n_h, mat_img, mat_image.size)
                 pbar.update(n_v*n_split+n_h)
 
         gen_image.save(output_image_name)
-                
 
-    def trim_into_square(self, img):
-        square_size = min(img.size)
-        width, height = img.size
-
-        if width > height:
-            top = 0
-            bottom = square_size
-            left = (width - square_size) / 2
-            right = left + square_size
-            box = (left, top, right, bottom)
-        else:
-            left = 0
-            right = square_size
-            top = (height - square_size) / 2
-            bottom = top + square_size
-            box = (left, top, right, bottom)
-
-        img = img.crop(box)
-
-        thumbnail_size = (square_size, square_size)
-        img.thumbnail(thumbnail_size)
-
-        return img
-
-
-    def get_near_image(self, r, g, b, threshold=10):
+    def get_near_image(self, r, g, b, threshold):
         """
         指定のRGB値からもっとも近い画像を返す
         しきい値内に収まっている画像が複数存在する場合はランダムで返す
@@ -88,8 +61,11 @@ class PhotoMosaicGenerator:
 
         candidates.sort(key=lambda x:x[1])
 
+        most_near_diff = candidates[0][1]
         for idx, m in enumerate(candidates):
-            if m[1] > threshold:
+            if (m[1]-most_near_diff) > threshold:
                 break
-        return candidates[np.random(idx)][0]
+        if idx == 0:
+            idx = 1
+        return candidates[np.random.randint(idx)][0]
 
--- mosaipy/preprocess.py
+++ mosaipy/preprocess.py
@@ -1,26 +1,13 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, REAL
-import glob
-import os, imghdr, shutil
 from PIL import Image, ImageStat
 from progressbar import ProgressBar
+import mosaipy.util
 
 class ImportImages:
 
     def __init__(self, dbname: str, img_dir_path: str):
         self._dbname = dbname
         self._img_dir_path = img_dir_path
-
-    def _get_image_names(self):
-        print('searching images...')
-        img_names = list()
-        for (root, dirs, files) in os.walk(self._img_dir_path): # 再帰的に探索
-            for f in files: # ファイル名だけ取得
-                target = os.path.join(root,f).replace("\\", "/")  # フルパス取得
-                if os.path.isfile(target): # ファイルかどうか判別
-                    if imghdr.what(target) != None : # 画像ファイルかどうかの判別
-                        img_names.append(target) # 画像ファイルであればリストに追加
-        print('got images')
-        return img_names
 
     def _create_table(self):
         self._engine = create_engine('sqlite:///'+self._dbname, echo=False)
@@ -38,30 +25,6 @@ class ImportImages:
 
         self._metadata.create_all()
 
-    def trim_into_square(self, img):
-        square_size = min(img.size)
-        width, height = img.size
-
-        if width > height:
-            top = 0
-            bottom = square_size
-            left = (width - square_size) / 2
-            right = left + square_size
-            box = (left, top, right, bottom)
-        else:
-            left = 0
-            right = square_size
-            top = (height - square_size) / 2
-            bottom = top + square_size
-            box = (left, top, right, bottom)
-
-        img = img.crop(box)
-
-        thumbnail_size = (square_size, square_size)
-        img.thumbnail(thumbnail_size)
-
-        return img
-
     def calc_mean(self):
         self._create_table()
         image_names = self._get_image_names()
@@ -75,12 +38,8 @@ class ImportImages:
                 img = Image.open(img_name)
             except OSError as e:
                 continue
-            # 透過画像の場合は一度RGBAに変換
-            if "transparency" in img.info:
-                img = img.convert("RGBA")
-            img = img.convert("RGB")
-            # 画像を正方形に切り取る
-            img = self.trim_into_square(img)
+            img = mosaipy.util.convert_to_rgb_image(img)
+            img = mosaipy.util.trim_into_square(img)
             stat = ImageStat.Stat(img)
             # tmp_insert = [img_name, stat.mean[0], stat.mean[1], stat.mean[2]]
             # inserts.append(tmp_insert)
