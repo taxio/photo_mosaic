4794e95a1036cd83bde1af3777b576310ba9847b
taxio <taxio.2029@gmail.com>
2018-03-23 20:41:47 +0900

    テストコード修正

--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from nose.tools import eq_
+import sqlite3
 
 from PIL import Image
 from photo_mosaic import ImportImages
@@ -14,6 +15,11 @@ class TestPreprocess(TestCase):
         self.import_image.drop_table()
 
     def test_calc_mean(self):
+        # データベース更新
         self.import_image.calc_mean()
 
-
+        conn = sqlite3.connect('./test/test.sqlite3')
+        c = conn.cursor()
+        c.execute('select * from materials')
+        materials = c.fetchall()
+        eq_(6, len(materials))
