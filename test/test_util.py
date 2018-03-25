c64d52263efd4db65e819fc066a0ea313c33fe5c
taxio <taxio.2029@gmail.com>
2018-03-25 09:34:59 +0900

    refactor generator

--- test/test_util.py
+++ test/test_util.py
@@ -9,7 +9,7 @@ class TestUtil(TestCase):
     """ Test case for util module """
 
     def test_get_image_names(self):
-        eq_(6, len(photo_mosaic.util.get_image_names('./test')))
+        eq_(7, len(photo_mosaic.util.get_image_names('./test')))
 
     def test_convert_to_rgb_image(self):
         img_names = photo_mosaic.util.get_image_names('./test')
